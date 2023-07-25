7d45beb653ddf640b95656281fdc2f3ee4dc2d43
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-07-25 11:49:31 +0300

    login_component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,9 @@ import { MainPageComponent } from './main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  @ViewChild("pass") pass!: ElementRef<HTMLInputElement>
+  loginForm!: FormGroup;
+ 
+  constructor(private router: Router) {
+ 
+  }
+ 
+  submitLogin() {
+   console.log(this.loginForm.value);
+   if(this.loginForm.get("password")?.invalid && this.loginForm.get("password")?.touched) {
+     this.pass.nativeElement.style.backgroundColor= "#ffcc00"
+   }
+   if(this.loginForm.get("password")?.valid) {
+     this.router.navigate(['/main'])
+   }
+   
+  }
+ 
+  ngOnInit(): void {
+      this.loginForm = new FormGroup({
+       "login": new FormControl("",[Validators.required, Validators.minLength(3)]),
+       "password": new FormControl("",[Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)])
+      })
+  }
 }
