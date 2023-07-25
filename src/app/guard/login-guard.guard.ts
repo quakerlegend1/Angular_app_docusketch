c2748e218bbd3f5f837e1adf0c6d0f455319bedb
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-07-26 00:56:55 +0300

    qq

--- src/app/guard/login-guard.guard.ts
+++ src/app/guard/login-guard.guard.ts
@@ -1,13 +1,15 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../authService/auth.service';
-
+import { Router } from '@angular/router';
 
 export const loginGuardGuard: CanActivateFn = (route, state) => {
+  const router: Router = inject(Router)
   const auth = inject(AuthService);
   if(auth.isAuth){
     return true
   } else {
+    router.navigate(["login"])
     return false
   }
   
