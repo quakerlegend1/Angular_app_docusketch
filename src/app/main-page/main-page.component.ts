76c4daf19aa72fb3dcc2cf6939bf51cd9ba48d54
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-07-26 22:37:20 +0300

    category filter
    qq

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -9,10 +9,25 @@ import { NewsDataService } from './news-data.service';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-  
 
-  constructor(private news: NewsDataService) {}
+  constructor(private news: NewsDataService) { }
 
-  newsData = this.news.getData() ;
+  newsData = this.news.getData();
+
+  selectedCategory: string = "all";
+
+  filterNews() {
+    if (this.selectedCategory === 'all') {
+      return this.newsData
+    } else if (this.selectedCategory === 'Angular') {
+      return this.newsData.filter((item) => item.category === "Angular")
+    } else if (this.selectedCategory === 'React') {
+      return this.newsData.filter((item) => item.category === "React")
+    } else if (this.selectedCategory === 'Vue') {
+      return this.newsData.filter((item) => item.category === "Vue")
+    }
+    return
+  }
+
 
 }
